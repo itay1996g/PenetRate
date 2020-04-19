ad513dcab093d9a5e26ee7ada2a2bf1e98fb9ead
itay1996g <itay1996g@gmail.com>
2020-04-19 19:13:52 +0300

    Crawler Module

--- PenetRate/modules/Crawler/crawler.py
+++ PenetRate/modules/Crawler/crawler.py
@@ -1,10 +1,11 @@
-import requests
 import re
 import os
+import json
 import time
 import random
 import socket
 import argparse
+import requests
 from bs4 import BeautifulSoup
 from fp.fp import FreeProxy
 from urllib.parse import urlparse
@@ -12,20 +13,23 @@ from urllib.parse import urlparse
 # Local Consts
 RESULTS_DIR_PATH  = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) + r"/Results"
 CRAWLER_RESULTS_PATH = RESULTS_DIR_PATH + r"/Crawler"
+EMAIL_REGEX = '[\w\.=-]+@[\w\.-]+\.[\w]{2,3}'
+IP_ADDRESS_REGEX = '(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
 
 class Crawler(object):
     def __init__(self, uid, starting_url):
-        self.starting_url = starting_url
-        self.visited = set() 
-        self.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+        self._starting_url = starting_url
+        self._visited = set() 
+        self._user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
         parsed = urlparse(starting_url)
-        self.domain_name = f"{parsed.scheme}://{parsed.netloc}"
-        self.proxy = FreeProxy().get()
-        self.user_id = uid
-        self.make_results_dir(RESULTS_DIR_PATH)
-        self.make_results_dir(CRAWLER_RESULTS_PATH)
+        self._domain_name = f"{parsed.scheme}://{parsed.netloc}"
+        self._proxy = FreeProxy().get()
+        self._user_id = uid
+        self._make_results_dir(RESULTS_DIR_PATH)
+        self._make_results_dir(CRAWLER_RESULTS_PATH)
+        self._personal_info = []
         
-    def random_sleep(self):
+    def _random_sleep(self):
         return random.random()
     
     def get_html(self, url):    
@@ -34,12 +38,13 @@ class Crawler(object):
         while html is None:
             try:
                 html = requests.get(url,
-                                    headers={"User-Agent":self.user_agent},
+                                    headers={"User-Agent":self._user_agent},
                                     timeout=5,
-                                    proxies={'http': self.proxy})
-                time.sleep(self.random_sleep())
+                                    proxies={'http': self._proxy})
+                self.extract_info(html)
+                time.sleep(self._random_sleep())
             except socket.gaierror:
-                self.proxy = FreeProxy().get()
+                self._proxy = FreeProxy().get()
             except Exception as e:
                 raise ValueError(str(e))
             
@@ -51,8 +56,9 @@ class Crawler(object):
             soup = BeautifulSoup(html, "html.parser")
             href_tags = soup.find_all(href=True)
             hrefs = [tag.get('href') for tag in href_tags]
-        except:
+        except Exception as e:
             return set()
+        
         parsed = urlparse(url) 
         base = f"{parsed.scheme}://{parsed.netloc}"
         
@@ -73,38 +79,60 @@ class Crawler(object):
         parsed = urlparse(url) 
         base = f"{parsed.scheme}://{parsed.netloc}"
 
-        if base == self.domain_name:
+        if base == self._domain_name:
             return True
 
         return False
+
+    def add_regex_results(self, regex, category, html):
+        JSON_OUTPUT_FORMAT = {'Name': None,
+                              'Value': None,
+                              'URL': None}
         
-    def extract_info(self, html):  
-        return None    
+        regex_result = re.findall(regex, html.text)
+
+        if len(regex_result) > 0:
+            JSON_OUTPUT_FORMAT['Name'] = category
+            JSON_OUTPUT_FORMAT['Value'] = regex_result
+            JSON_OUTPUT_FORMAT['URL'] = html.url
+
+            if JSON_OUTPUT_FORMAT not in self._personal_info:
+                self._personal_info.append(JSON_OUTPUT_FORMAT)
+        
+    def extract_info(self, html):
+        self.add_regex_results(EMAIL_REGEX, 'Email', html)
+        self.add_regex_results(IP_ADDRESS_REGEX, 'IP Address', html)
 
     def crawl(self, url):
         """
         The loop iterates through the source code of the site for dry dirbust.
         """
         for link in self.get_links(url):
-            if link in self.visited:    
+            if link in self._visited:    
                 continue
-            self.visited.add(link)
+            self._visited.add(link)
             self.crawl(link)
 
-    def make_results_dir(self, path):
+    def _make_results_dir(self, path):
         if not os.path.exists(path):
             os.mkdir(path)
             
     def save_results(self):
         try:
-            with open(CRAWLER_RESULTS_PATH + r'/{}.txt'.format(self.user_id), 'w') as output_file:
-                for link in self.visited:
+            # Saving all of the URLs retrived
+            with open(CRAWLER_RESULTS_PATH + r'/{}.txt'.format(self._user_id), 'w') as output_file:
+                for link in self._visited:
                     output_file.write(link + '\n')
-        except:
-            return
+
+            # Saving all of the data extracted from source codes
+            with open(CRAWLER_RESULTS_PATH + r'/{}_extract_info.txt'.format(self._user_id), 'w') as output_file:
+                for info in self._personal_info:
+                    output_file.write(str(info) + '\n')
+        except Exception as e:
+            raise e
             
     def scan(self):             
-        self.crawl(self.starting_url)
+        self.crawl(self._starting_url)
         self.save_results()
 
 def get_args():
