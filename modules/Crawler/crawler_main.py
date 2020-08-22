a192bbb42a0f5e0e77482765352248f87b927d50
itay1996g <itay1996g@gmail.com>
2020-08-22 13:20:01 +0300

    Added dirbust flag in order to run right after the crawler

--- modules/Crawler/crawler_main.py
+++ modules/Crawler/crawler_main.py
@@ -1,7 +1,9 @@
 import argparse
 from crawler import *
 
-CRAWLER_TABLE_NAME = r"clientside_scan"
+CLIENTSIDE_TABLE_NAME = r"clientside_scan"
+CRAWLER_TABLE_NAME = r"crawler_scan"
+VULNSCAN_TABLE_NAME = r"generalvulnerabilities_scan"
 
 def get_args():
   """
@@ -13,12 +15,18 @@ def get_args():
   parser.add_argument('-u','--uid', help='User ID', required=True)
   parser.add_argument('-c','--auth-cookie', help='Authenticated Cookie', required=False, default=None)
   parser.add_argument('-a','--attack', help='Attack Mode', required=False, default=False, action='store_true')
+  parser.add_argument('-b','--dirbust', help='Run with DirBust', required=False, default=False, action='store_true')
+  parser.add_argument('-f','--wordlist', help='Wordlist File', required=False)
 
   args = vars(parser.parse_args())
 
   if args['attack']:
     if not args['auth_cookie']:
       parser.error('In attack mode user must supply authenticated cookie')
+  
+  if args['dirbust']:
+    if not args['wordlist']:
+        parser.error('Wordlist must be specified in order to run dirbust')
 
   return args
 
@@ -30,6 +38,7 @@ def route_crawl(args):
     3. Run AuthBypass Scanner
     4. Run SQLInjection Scanner
   """
+  
   with Crawler(base_url=args['domain'], attack_mode=False, auth_cookie=None) as crawler:
     crawler.crawl()
     crawler.save_results(args['uid'])
@@ -53,6 +62,13 @@ def route_crawl(args):
     
     authbypass_scanner.scan()
     sqli_scanner.scan()
+    
+  if args['dirbust']:
+      sys.path.append(os.path.abspath(os.path.join(__file__, os.pardir)) + '/../DirBust')
+      dirbust_module = __import__("dirbust")
+      scanner = dirbust_module.DirBuster(args['domain'], args['wordlist'], args['uid'])
+      scanner.scan()
+      send_to_api(args['uid'], dirbust_module.DIRBUST_TABLE_NAME)
 
 def main():
   args = get_args()
@@ -62,6 +78,10 @@ def main():
   make_results_dir(SENSITIVEINFO_RESULTS_PATH)
   
   route_crawl(args)
+  
+  send_to_api(args['uid'], CRAWLER_TABLE_NAME)
+  send_to_api(args['uid'], CLIENTSIDE_TABLE_NAME)
+  send_to_api(args['uid'], VULNSCAN_TABLE_NAME)
 
 if __name__ == '__main__':
   main()
